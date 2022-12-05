323a3c68778c94497d2f7986d5a36f8650f05bad
dkdam <dkdam@live.com>
2022-12-05 22:29:57 +1100

    adding navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className='navbar'>
+            <a href='/'>Home</a>
+            <a href='/About'>About</a>
+            <a href='/Team'>Team</a>
+            <a href='/Contact'>Contact</a>
+        </div>
+    )
+}
+
+export default Navbar
