ff1a70de14960c5eaa307d206c43cfa61d357d9c
dkdam <dkdam@live.com>
2022-12-06 23:18:45 +1100

    added more components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import About from './Components/About';
+import Contact from './Components/Contact';
 import MainPage from './Components/MainPage';
 import Navbar from './Components/Navbar';
+import Team from './Components/Team';
 
 function App() {
   return (
     <div className="App">
       < Navbar />
       < MainPage />
+      < About />
+      < Team />
+      < Contact />
     </div>
   );
 }
