78c110ce0ade20a5d8634002e9173538b99782e7
dkdam <dkdam@live.com>
2022-12-09 22:02:57 +1100

    created a counter button on mainPage

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
 import '../Styles/MainPage.css'
 
 function MainPage() {
+    const [counter, setCounter] = useState(0)
+
+    const increase = () => {
+        setCounter(count => count + 1)
+    }
+
+    const decrease = () => {
+        if ( counter > 0 ) {
+            setCounter(count => count - 1)
+        }
+    }
+
+    const reset = () => {
+        setCounter(0)
+    }
+
     return (
         <div className='mainPage'>
-            <h1>Welcome</h1>
-            <button>Click Me</button>
+            <h1>Welcome Home</h1>
+            <h2>Counter</h2>
+            <span className="output">{ counter }</span>
+            <div className="btnContainer">
+                <button className='btn' onClick={ increase }>+</button>
+                <button className='btn' onClick={ decrease }>-</button>
+                <button className='reset' onClick={ reset }>reset</button>
+            </div>
         </div>
     )
 }
