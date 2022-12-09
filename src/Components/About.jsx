0831906dd6574ca648b57ed9657861660ad40a82
dkdam <dkdam@live.com>
2022-12-09 22:55:07 +1100

    setting up the about scss

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import "../Styles/About.css"
 
 const About = () => {
     return (
-        <div>
+        <div className="about">
             <h1>About</h1>
+            <p>I'd like to order one large phone with extra phones please. cell phone, no no no rotary... and payphone on half. A price for everything. Hey, what the fuck, Morty? The train you got me just completely derailed itself. There's blood all over the windows. You're young, you have your whole life ahead of you, and your anal cavity is still taut yet malleable. </p>
         </div>
     )
 }
