e8149ddf298d33866475c86d78758db4dafb159d
dkdam <dkdam@live.com>
2022-12-12 19:54:03 +1100

    adding more info on about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,7 +5,29 @@ const About = () => {
     return (
         <div className="about">
             <h1>About</h1>
-            <p>I'd like to order one large phone with extra phones please. cell phone, no no no rotary... and payphone on half. A price for everything. Hey, what the fuck, Morty? The train you got me just completely derailed itself. There's blood all over the windows. You're young, you have your whole life ahead of you, and your anal cavity is still taut yet malleable. </p>
+            <section>
+                <h2>About Me</h2>
+                <div className="profile">
+                    <div className="paragraph">
+                        <p>I'd like to order one large phone with extra phones please. cell phone, no no no rotary... and payphone on half. A price for everything. Hey, what the fuck, Morty? The train you got me just completely derailed itself. There's blood all over the windows. You're young, you have your whole life ahead of you, and your anal cavity is still taut yet malleable. </p>
+
+                        <p>Oh, yeah! We're like Luke and Leia! ...Uh, except no kissing part. What's another famous brother–sister team? I wish that shotgun was my penis. If anything happened to you I would kill myself! I love you bad, bobo! Nice to wheat you! 
+                        </p>
+                    </div>
+                <img src="http://placebear.com/200/200" alt="" />
+                </div>
+            </section>
+            <section>
+                <h2>About Me More</h2>
+                <div className="profile">
+                    <img src="http://placebear.com/200/300" alt="" />
+                    <div className="paragraph">
+                        <p>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother’s keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.</p>
+
+                        <p>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don’t know exactly when we turned on each other, but I know that seven of us survived the slide… and only five made it out. Now we took an oath, that I’m breaking now. We said we’d say it was the snow that killed the other two, but it wasn’t. Nature is lethal but it doesn’t hold a candle to man.</p>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
