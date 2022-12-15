b9de2ddb7a768021261565a110241283b58b59f3
dkdam <dkdam@live.com>
2022-12-15 21:00:16 +1100

    fetched data

--- src/Components/Fetch.jsx
+++ src/Components/Fetch.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
 
 function Fetch() {
+    const [quote, setQuote] = useState("")
+
+    const fetchQuote = () => {
+        Axios.get("https://api.kanye.rest/").then((res) => {
+            setQuote(res.data.quote)
+        })
+    }
+
+    useEffect(() => {
+        fetchQuote()
+    }, [])
+
     return (
         <div>
             <h1>Fetch Some Data</h1>
+            <button onClick={fetchQuote}>Kanye West</button>
+            <p> {quote} </p>
         </div>
     )
 }
