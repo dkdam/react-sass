c876e6d44097da95eeaf183007eaca09d6fb6764
dkdam <dkdam@live.com>
2022-12-15 21:29:36 +1100

    fetching some names

--- src/Components/Fetch.jsx
+++ src/Components/Fetch.jsx
@@ -3,23 +3,33 @@ import Axios from 'axios'
 import '../Styles/Fetch.css'
 
 function Fetch() {
-    const [quote, setQuote] = useState("")
 
-    const fetchQuote = () => {
-        Axios.get("https://api.kanye.rest/").then((res) => {
-            setQuote(res.data.quote)
+    const [name, setName] = useState("")
+    const [predictAge, setPredictedAge] = useState(null)
+
+    const fetchData = () => {
+        Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+            setPredictedAge(res.data)
         })
     }
-
-    useEffect(() => {
-        fetchQuote()
-    }, [])
 
     return (
         <div className='fetch'>
             <h1>Fetch Some Data</h1>
-            <button onClick={fetchQuote}>Kanye West</button>
-            <p> {quote} </p>
+
+            <input 
+                placeholder='Type a name...' 
+                onChange={(event) => {
+                    setName(event.target.value);
+                }}
+            /> 
+            <br />
+            <button onClick={fetchData}>Predict Age</button>
+
+            <h2> Name: {predictAge?.name}</h2>
+            <h2> Age: {predictAge?.age}</h2>
+            <h2> Count: {predictAge?.count}</h2>
+
         </div>
     )
 }
