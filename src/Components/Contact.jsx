d67a766747db8f1ae41ad9155cd2627e6ff18cfb
dkdam <dkdam@live.com>
2022-12-19 15:23:15 +1100

    added some css to contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import '../Styles/Contact.css'
 
 const Contact = () => {
     return (
-        <div>
+        <div className="contact">
             <h1>Contact me</h1>
         </div>
     )
