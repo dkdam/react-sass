8874f8330b8bdf9825b322bc6822a43bce842c56
dkdam <dkdam@live.com>
2022-12-19 17:44:51 +1100

    setting up axios

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import '../Styles/Contact.css'
+import axios from "axios";
 
 const Contact = () => {
+
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+    .then((res) => {
+        const data = res.data
+        console.log(data)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
     return (
         <div className="contact">
-            <h1>Contact me</h1>
+            <h1>Contacts</h1>
         </div>
     )
 }
