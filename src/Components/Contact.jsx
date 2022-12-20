bd87910515216e70c0a826075436135f9f00c045
dkdam <dkdam@live.com>
2022-12-20 14:26:15 +1100

    listed contacts

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,21 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import '../Styles/Contact.css'
 import axios from "axios";
 
 const Contact = () => {
 
+    const [contact, setContact] = useState([])
+
+useEffect(() => {
     axios.get(`https://jsonplaceholder.typicode.com/users`)
     .then((res) => {
         const data = res.data
-        console.log(data)
+        setContact(data)
     })
     .catch(error => {
         console.log(error)
     })
+},[])
 
     return (
         <div className="contact">
             <h1>Contacts</h1>
+            <ul>
+                {
+                    contact.map(keys => <li key = {keys.id} >{keys.name}</li>)
+                }
+            </ul>
         </div>
     )
 }
